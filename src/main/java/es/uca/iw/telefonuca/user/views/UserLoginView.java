6d93ceda591631c57926aeef1d191cadcc07a92a
David Brea Piñero <100689419+davidbp10@users.noreply.github.com>
2024-01-03 17:34:16 +0100

    Update UserLoginView.java

--- src/main/java/es/uca/iw/telefonuca/user/views/UserLoginView.java
+++ src/main/java/es/uca/iw/telefonuca/user/views/UserLoginView.java
@@ -2,38 +2,48 @@ package es.uca.iw.telefonuca.user.views;
 
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.internal.RouteUtil;
-import com.vaadin.flow.server.VaadinService;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 import es.uca.iw.telefonuca.user.security.AuthenticatedUser;
+import es.uca.iw.telefonuca.config.TranslationProvider;
+
+import java.util.Locale;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 @AnonymousAllowed
 @PageTitle("Login")
 @Route(value = "login")
-public class UserLoginView extends LoginOverlay implements BeforeEnterObserver {
+public class UserLoginView extends LoginOverlay implements BeforeEnterObserver, LocaleChangeObserver {
 
     private final AuthenticatedUser authenticatedUser;
+    private final TranslationProvider translationProvider;
 
-    public UserLoginView(AuthenticatedUser authenticatedUser) {
+    public UserLoginView(AuthenticatedUser authenticatedUser, TranslationProvider translationProvider) {
         this.authenticatedUser = authenticatedUser;
+        this.translationProvider = translationProvider;
+
         setAction(RouteUtil.getRoutePath(VaadinService.getCurrent().getContext(), getClass()));
 
-        LoginI18n i18n = LoginI18n.createDefault();
+        final Locale currentLocale = LocaleContextHolder.getLocale();
+        final LoginI18n i18n = LoginI18n.createDefault();
         i18n.setHeader(new LoginI18n.Header());
         i18n.getHeader().setTitle("TelefonUCA");
-        i18n.getHeader().setDescription("Login using admin/admin or with any other credentials");
+        i18n.getHeader().setDescription(translationProvider.getTranslation("userLogin.description", currentLocale));
         i18n.setAdditionalInformation(null);
         setI18n(i18n);
 
-        setForgotPasswordButtonVisible(false);
+        setForgotPasswordButtonVisible(true);
         setOpened(true);
     }
-
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
@@ -44,5 +54,16 @@ public class UserLoginView extends LoginOverlay implements BeforeEnterObserver {
         }
 
         setError(event.getLocation().getQueryParameters().getParameters().containsKey("error"));
+    }
+
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        final Locale currentLocale = LocaleContextHolder.getLocale();
+        final LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setHeader(new LoginI18n.Header());
+        i18n.getHeader().setTitle("TelefonUCA");
+        i18n.getHeader().setDescription(translationProvider.getTranslation("userLogin.description", currentLocale));
+        i18n.setAdditionalInformation(null);
+        setI18n(i18n);
     }
 }
