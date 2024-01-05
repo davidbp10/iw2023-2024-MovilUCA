4d42464f68721cd8970cd2f15747832b239507ef
cainama <lolozxt@gmail.com>
2024-01-05 01:09:20 +0100

    V1.2024.01.04: integración de componentes mejores para vistas en el pom.xml

--- src/main/dev-bundle/webapp/VAADIN/build/vaadin-details-bf336660-5961f636.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./indexhtml-799c0327.js";const t={tagName:"vaadin-details",displayName:"Details",elements:[{selector:"vaadin-details",displayName:"Root element",properties:e}]};export{t as default};
